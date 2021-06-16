d1fca96f00db22783e70fd5c585ce224e5dfdacf
osymandius <bolistevens@gmail.com>
2021-06-16 20:14:53 +0100

    SSA update

--- src/aaa_assign_populations/script.R
+++ src/aaa_assign_populations/script.R
@@ -7,14 +7,57 @@ spectrum <- extract_pjnz_naomi("depends/spectrum_file.zip")
 merge_cities <- read_sf("merge_cities.geojson")
 pse <- read.csv("pse.csv")
 
-# pse <- read.csv("src/aaa_assign_populations/pse.csv")
-# iso3_c <- "BFA"
-# areas <- read_sf("archive/aaa_inputs_orderly_pull/20210428-104119-ff9e16d2/naomi_areas.geojson") %>%
-#   mutate(iso3 = iso3_c)
-# population <- read_csv("archive/aaa_inputs_orderly_pull/20210428-104119-ff9e16d2/naomi_population.csv")
-# spectrum <- extract_pjnz_naomi("archive/aaa_spectrum_adr_pull/20210428-113350-8fc632fa/spectrum_file.zip")
-# merge_cities <- read_sf("src/aaa_assign_populations/merge_cities.geojson")
-browser()
+population <- population %>%
+  left_join(get_age_groups() %>% select(age_group, age_group_sort_order)) %>%
+  filter(age_group_sort_order %in% 16:22) %>%
+  group_by(area_id, area_name, calendar_quarter, sex) %>%
+  summarise(population = sum(population)) %>%
+  mutate(age_group = "Y015_049",
+         source = "Naomi") %>%
+  ungroup() %>%
+  separate(calendar_quarter, remove=FALSE, sep = c(2,6), into=c(NA, "year", NA), convert = TRUE) %>%
+  filter(year == floor(median(year)))
+
+naomi_pop_year <- unique(population$year)
+
+if(unique(spectrum$spectrum_region_code)) {
+  spectrum <- spectrum %>%
+    left_join(areas %>% 
+                filter(area_level == 1) %>%
+                select(spectrum_region_code, area_id, area_level) %>% 
+                st_drop_geometry()
+    )
+  
+  working_level <- 1
+  stop("Subnational file!!")
+} else {
+  spectrum <- spectrum %>%
+    left_join(areas %>% 
+                filter(area_level == 0) %>%
+                select(spectrum_region_code, area_id, area_level) %>% 
+                st_drop_geometry()
+    )
+  working_level <- 0
+}
+
+spectrum <- spectrum %>%
+  filter(age %in% 15:49) %>%
+  group_by(area_id, area_level, sex, year) %>%
+  summarise(population = sum(totpop)) %>%
+  group_by(area_id, sex) %>%
+  mutate(age_group = "Y015_049") %>%
+  ungroup()
+
+spectrum_population_change <- spectrum %>%
+  mutate(change = population/population[year==2010],
+         source = "WorldPop") %>%
+  bind_rows(
+    spectrum %>%
+      mutate(change = population/population[year==naomi_pop_year],
+             source = "Naomi")
+  )
+
+
 cities_areas <- merge_cities %>%
   bind_rows(areas)
 
@@ -36,7 +79,7 @@ pse_areas <- pse %>%
 min_dist <- pse_areas %>%
   filter(iso3 == iso3_c) %>%
   full_join(cities_areas %>% filter(iso3 == iso3_c), by="iso3")  %>%
-  mutate(dist = stringdist::stringdist(value, area_name)) %>%
+  mutate(dist = stringdist::stringdist(value, tolower(area_name))) %>%
   group_by(idx) %>%
   filter(dist == min(dist)) 
 
@@ -67,15 +110,20 @@ if(nrow(level_check)) {
 }
 
 bad_match <- min_dist %>%
-  filter(n() == 1, dist>=3)
+  filter(dist>=3)
 
 if (nrow(bad_match)) {
   bad_match_error <- bad_match %>%
-    select(given_area, area_name, area_id, dist) %>%
-    ungroup
+    ungroup %>%
+    mutate(iso3 = iso3_c) %>%
+    select(iso3, given_name = value, attempted_match = area_name, attempted_area_id = area_id, string_distance = dist)
   
-  stop("\nString match is bad:\n", 
+  warning("\nString match is bad:\n", 
           paste0(utils::capture.output(bad_match_error), collapse = "\n"))
+  
+  
+} else {
+  bad_match_error <- data.frame(iso3 = iso3_c, x = "No bad matches")
 }
 
 pop_search <- best_matches %>%
@@ -85,88 +133,54 @@ pop_search <- best_matches %>%
          area_id = paste0(iso3_c, "_city_", city_num)
   )
 
-pop_search_unique <- pop_search %>%
-  filter(!area_id %in% areas$area_id) %>%
-  select(area_id, geometry) %>%
-  distinct() %>%
-  mutate(source = "WorldPop")
-
-class(pop_search_unique) <- c("sf", "tbl_df", "tbl", "data.frame")
-attr(pop_search_unique, "sf_column") <- "geometry"
-
-pop_search_res <- extract_kp_worldpop(pop_search_unique, iso3_c) 
+if(nrow(pop_search)) {
+  
+  pop_search_unique <- pop_search %>%
+    filter(!area_id %in% areas$area_id) %>%
+    select(area_id, geometry) %>%
+    distinct() %>%
+    mutate(source = "WorldPop")
+  
+  class(pop_search_unique) <- c("sf", "tbl_df", "tbl", "data.frame")
+  attr(pop_search_unique, "sf_column") <- "geometry"
+  
+  pop_search_res <- extract_kp_worldpop(pop_search_unique, iso3_c) 
+  
+  extrapolate_id <- pop_search_unique %>%
+    select(area_id, source) %>%
+    bind_rows(
+      best_matches %>%
+        filter(area_id %in% areas$area_id) %>%
+        select(area_id) %>%
+        distinct() %>%
+        mutate(source = "Naomi")
+    ) %>% 
+    st_drop_geometry()
+  
+  merged_populations <- pop_search_res %>%
+    select(area_id, sex, population) %>%
+    bind_rows(
+      population %>%
+        select(area_id, sex, population)
+    )
+  
+} else {
+  
+  extrapolate_id <- best_matches %>%
+    filter(area_id %in% areas$area_id) %>%
+    select(area_id) %>%
+    distinct() %>%
+    mutate(source = "Naomi")
+  
+  merged_populations <- population %>%
+    select(area_id, sex, population)
+  
+}
 
 #### Extrapolate city populations
 
-if(unique(spectrum$spectrum_region_code)) {
-  spectrum <- spectrum %>%
-    left_join(areas %>% 
-                filter(area_level == 1) %>%
-                select(spectrum_region_code, area_id, area_level) %>% 
-                st_drop_geometry()
-    )
-  
-  working_level <- 1
-  stop("Subnational file!!")
-} else {
-  spectrum <- spectrum %>%
-    left_join(areas %>% 
-                filter(area_level == 0) %>%
-                select(spectrum_region_code, area_id, area_level) %>% 
-                st_drop_geometry()
-    )
-  working_level <- 0
-}
-
-spectrum <- spectrum %>%
-  filter(age %in% 15:49) %>%
-  group_by(area_id, area_level, sex, year) %>%
-  summarise(population = sum(totpop)) %>%
-  group_by(area_id, sex) %>%
-  mutate(age_group = "Y015_049") %>%
-  ungroup()
-
 ## Aggregate Naomi populations
 
-population <- population %>%
-  left_join(get_age_groups() %>% select(age_group, age_group_sort_order)) %>%
-  filter(age_group_sort_order %in% 16:22) %>%
-  group_by(area_id, area_name, calendar_quarter, sex) %>%
-  summarise(population = sum(population)) %>%
-  mutate(age_group = "Y015_049",
-         source = "Naomi") %>%
-  ungroup() %>%
-  separate(calendar_quarter, remove=FALSE, sep = c(2,6), into=c(NA, "year", NA), convert = TRUE) %>%
-  filter(year == median(year))
-
-naomi_pop_year <- unique(population$year)
-
-extrapolate_id <- pop_search_unique %>%
-  select(area_id, source) %>%
-  bind_rows(
-    best_matches %>%
-      filter(area_id %in% areas$area_id) %>%
-      select(area_id) %>%
-      distinct() %>%
-      mutate(source = "Naomi")
-  ) %>% 
-  st_drop_geometry()
-
-spectrum_population_change <- spectrum %>%
-  mutate(change = population/population[year==2010],
-         source = "WorldPop") %>%
-  bind_rows(
-    spectrum %>%
-      mutate(change = population/population[year==naomi_pop_year],
-             source = "Naomi")
-  )
-
-merged_populations <- pop_search_res %>%
-  select(area_id, sex, population) %>%
-  bind_rows(
-    population %>%
-      select(area_id, sex, population)
-  )
 
 extrapolated_populations <- crossing(extrapolate_id,
                                      sex = c("female", "male"),
@@ -196,14 +210,17 @@ row_populations <- pop_search %>%
     kp == "MSM" ~ "male",
     kp %in% c("FSW", "SW") ~ "female"
   )) %>%
+  type.convert() %>%
   left_join(extrapolated_populations) %>%
   group_by(row_id) %>%
   summarise(population = sum(population))
 
 pse <- pse %>%
+  filter(iso3 == iso3_c) %>%
   left_join(row_populations) %>%
   filter(!is.na(population)) %>%
   select(iso3, kp, area_name, year, pse_lower, pse, pse_upper, population) %>%
   mutate(pse_prevalence = pse/population)
 
 write_csv(pse, "pse_prevalence.csv")
+write_csv(bad_match_error, "bad_match_error.csv")
--- src/aaa_inputs_orderly_pull/script.R
+++ src/aaa_inputs_orderly_pull/script.R
@@ -1,8 +1,9 @@
+
 # This script will pull the following artefacts from the orderly archive
 # for the downstream use in a parameterised orderly task:
 # * Areas: latest from orderly archive
-# * Population: latest from orderly, worldpop or gpw
-# * Survey: latest from ordelry archive
+# * Population: latest from orderly, worldpop or gpw (pull from org-iso-mapping in global resources)
+# * Survey: latest from orderly archive
 
 
 # Prepare model inputs
@@ -22,26 +23,29 @@ st_write(areas, "naomi_areas.geojson", delete_dsn = TRUE)
 
 
 ## Population files
+# Map iso3 parameter to population source
+map <- read_csv("resources/org_iso_mapping.csv")
+pop_source <- map$pop_data_source[map$iso3 == iso3]
 
 if(pop_source == "orderly") {
-
+  
   pop_file <- files[grepl(paste0(tolower(iso3), "_population"), files)]
   pop <- read_csv(paste0("depends/", pop_file))
-
+  
 } else if (pop_source == "gpw") {
-
+  
   pop <- read_csv("depends/population_gpw_naomi.csv")
-
+  
 } else if (pop_source == "worldpop") {
-
+  
   pop <- read_csv("depends/population_worldpop_naomi.csv")
-
+  
 }
 
 # Save population file
 write_csv(pop, "naomi_population.csv")
 
-# ## Survey files
+## Survey files
 # survey_file <- files[grepl(paste0(tolower(iso3), "_survey"), files)]
 # 
 # if (!basename(survey_file) %in% files) {
